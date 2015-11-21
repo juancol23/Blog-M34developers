3f3666a0f9f8133882b33fb646bd199c5d5665fa
Juan Valdemar Colichon Ramirez <juanvcr@outlook.com>
2015-11-21 00:31:39 -0500

    Blog
    Tratando codigo

---
+++ app/src/main/java/com/m34developers/blog/MainActivity.java
@@ -0,0 +1,62 @@
+package com.m34developers.blog;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.m34developers.blog.R;
+
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    // Atributos
+    ListView listView;
+    ArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Obtener instancia de la lista
+        listView= (ListView) findViewById(R.id.listView);
+
+        // Crear adaptador y setear
+        adapter = new PostAdapter(this);
+        listView.setAdapter(adapter);
+
+
+    }
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_buscar) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
